54cafa215bdb164a0fe54c2599c2dadd4dbfefd6
AreczeQ192 <askupien8@gmail.com>
2023-11-11 15:37:37 +0100

    explore component styles

--- components/Explore.tsx
+++ components/Explore.tsx
@@ -1,5 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, Image } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  FlatList,
+  StyleSheet,
+  Dimensions,
+} from "react-native";
 
 type ArtworkData = Array<{
   id: string;
@@ -20,7 +27,7 @@ export default function Explore() {
         const fields =
           "id,title,artist_display,date_display,image_id,dimensions,description";
         const response = await fetch(
-          `https://api.artic.edu/api/v1/artworks/?fields=${fields}&limit=5`
+          `https://api.artic.edu/api/v1/artworks/?fields=${fields}&limit=15`
         );
         const json = await response.json();
         setData(json.data);
@@ -32,29 +39,77 @@ export default function Explore() {
 
   return (
     <>
-      {data?.map((art, i) => {
-        return (
-          <View
-            key={i}
-            style={{
-              width: 320,
-              height: 80,
-              backgroundColor: "green",
-              flexDirection: "row",
-              alignItems: "center",
-            }}
-          >
-            <Image
-              source={{
-                uri: `https://www.artic.edu/iiif/2/${art.image_id}/full/600,/0/default.jpg`,
-              }}
-              resizeMode="contain"
-              style={{ width: 72, height: 72 }}
-            />
-            <Text>{art.title}</Text>
-          </View>
-        );
-      })}
+      <FlatList
+        data={data}
+        renderItem={({ item }) => {
+          return (
+            <View style={styles.flatlistItem}>
+              <View style={styles.artworkContainer}>
+                <View style={styles.imageContainer}>
+                  <Image
+                    source={{
+                      uri: `https://www.artic.edu/iiif/2/${item.image_id}/full/600,/0/default.jpg`,
+                    }}
+                    style={styles.artworkImage}
+                    resizeMode="contain"
+                  />
+                </View>
+                <View style={styles.artworkDataContainer}>
+                  <Text style={styles.artworkTitle}>{item.title}</Text>
+                  <Text style={styles.artworkArtist}>
+                    {item.artist_display.split("\n")[0]}
+                  </Text>
+                </View>
+              </View>
+            </View>
+          );
+        }}
+        showsVerticalScrollIndicator={false}
+        style={styles.flatlist}
+      />
     </>
   );
 }
+
+const screenWidth = Dimensions.get("window").width;
+const styles = StyleSheet.create({
+  container: {},
+  flatlist: {
+    flex: 1,
+    paddingHorizontal: 12,
+    // paddingVertical: 8,
+  },
+  flatlistItem: { paddingVertical: 4 },
+  imageContainer: {
+    backgroundColor: "deepskyblue",
+    borderRadius: 6,
+    overflow: "hidden",
+  },
+  artworkContainer: {
+    flex: 1,
+    height: 80,
+    paddingHorizontal: 4,
+    paddingVertical: 4,
+    backgroundColor: "green",
+    borderRadius: 8,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  artworkDataContainer: {
+    flex: 1,
+    paddingLeft: 8,
+    paddingRight: 4,
+    borderRadius: 14,
+  },
+  artworkImage: {
+    width: 72,
+    height: 72,
+  },
+  artworkTitle: {
+    fontSize: 14,
+    color: "#f2f2f2",
+  },
+  artworkArtist: {
+    fontSize: 10,
+  },
+});
